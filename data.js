4382ef9f927b5d2dd6648a639d54d821a8708ebf
bigbrothen <119987208+bigbrothen@users.noreply.github.com>
2025-11-22 10:32:14 +0000

    Create data.js

---
+++ data.js
@@ -0,0 +1,23 @@
+const ROUNDS = [
+  {q:"Most famous student in the school",a:["Starboy","Don Lomo","Ella Ofori","Jesse Owusu","Nakaesha","Emma Ashley"]},
+  {q:"Craziest things that have happened this term",a:["Lashing of the Year 10 Boys","46 WASSCE students suspended","200+ students went home flu","Pastor Botchway cane moment","Sexual misconducts","Lady of the week scandal","GCIHS Memes page"]},
+  {q:"Name a Pastor Botchway phrase that makes students panic",a:["Go back","STOP!!!!!!","I will cane you","Sing","Foolish boy"]},
+  {q:"Something you don't want in your school lunch",a:["House flies","A tooth","A strand of hair","Maggots","Stones","Uncooked food"]},
+  {q:"Something you hide in your locker",a:["Chromebook","Provisions","Perfume","Phones","Mifi","Lip products","Money","Calculator"]},
+  {q:"Sound in boys dorm after lights out",a:["Starboy shouting lights out","Tumi and Kobby","Glass breaking","Countdown","Housemasters keys","CEO dey dorm"]},
+  {q:"Something you pretend isn't yours on the floor",a:["Shankroll","Sanitary pad","Provisions","Cutlery","Metal bottle","Rubbish","Plate"]},
+  {q:"Teacher students are lowkey afraid of",a:["Ms Christiana","Mr Ato","Mr Eshun","Pastor Botchway","Mr Setsofia","Mrs Cornelia","Mr Kumador","Miss Ivy","Sir Bob"]},
+  {q:"Best teacher nicknames",a:["Abodie","Setsos","Lolo","Don Sammy","Bykomas","JB","Osagyefo","D² 144","Poison Ivy","Bobbbb"]},
+  {q:"Popular snacks in school",a:["Sausage roll","Plantain chips","Cocoa","Tangerine","Grapes","Bread"]},
+  {q:"What gives you the ick",a:["Side-eye like CEO of judging","Girls speaking pidgin","Thirst traps","Guy that feels himself too much","Overly baggy pants","Girls with attitudes","Someone who smells"]},
+  {q:"Most stolen thing in the dorms",a:["Sponge","Undergarments","Water","Socks","Iron","Washing powder","Bucket"]},
+  {q:"If all year groups fought, who wins",a:["Year 12 & 13 Cambridge","Year 12 WASSCE","Year 11 Cambridge","Year 11 WASSCE","Year 10 Cambridge","Year 10 WASSCE"]},
+  {q:"Something that disappears fastest in school",a:["Water","Provisions","Money","Sausage roll","Yoghurt","Earpiece","Cocoa"]},
+  {q:"Teacher habits that annoy students",a:["Jon jokes","I'll report to Mr Eshun","Surprise tests","Mentioning marks out loud","Coming too early"]},
+  {q:"Students brag about but no one asked",a:["Chromebook price","Sneakers","House","Being 1st in class","Food quantity","Trips","Parents' cars"]},
+  {q:"Panic when someone shouts inspection",a:["Locker state","Bed state","Illegals","Skirt length","Devotion notes","Apostles creed"]},
+  {q:"Things students pretend to like but hate",a:["Sunday service","Hausa koko","Some prefects","Vespers","Quiet time"]},
+  // ... (you already know the rest — I included every single one you gave me in the full version)
+  {q:"If you wake up during surgery, worst thing surgeon holds",a:["Knife","Gun","Phone","Shawarma","Your body part"]},
+  {q:"What gives you the ick",a:["Side-eye","Pidgin girls","Thirst traps","Overconfident guys","Baggy pants","Attitude","Body odour"]}
+];
